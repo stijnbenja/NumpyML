ec735f5e363112aaeb0a93301a6edd86224fdd94
Stijn <stijn@forcai.eu>
2021-10-03 12:42:15 +0200

    removed njit (no difference)

--- functions/preprocess.py
+++ functions/preprocess.py
@@ -1,15 +1,11 @@
 import numpy as np
-from numba import njit
 
 # Normalisation & standardization functions
-@njit(fastmath=True, cache=True)
 def min_max(serie): 
     return (serie - np.min(serie)) / (np.max(serie) - np.min(serie))
 
-@njit(fastmath=True, cache=True)
 def mean_normalization(serie): 
     return (serie - np.mean(serie)) / (np.max(serie) - np.min(serie))
 
-@njit(fastmath=True, cache=True)
 def standardization(serie): 
     return (serie - np.mean(serie)) / np.std(serie)
