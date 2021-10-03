d3f390c879993f3962ab7b5ceeb086b812e8e87c
Stijn <stijn@forcai.eu>
2021-10-03 12:29:00 +0200

    NJIT (hyperspeed) all functions

--- functions/activations.py
+++ functions/activations.py
@@ -1,10 +1,13 @@
 import numpy as np
+from numba import njit
 
+@njit(fastmath=True, cache=True)
 def sigmoid(x):
     ceiling = 100 #Against multiplication overflow
     big_out = np.where(x > ceiling, ceiling, x)
     small_out = np.where(big_out < -ceiling, -ceiling, big_out)
     return 1 / (1 + np.exp(-small_out))
 
+@njit(fastmath=True, cache=True)
 def dSigmoid(x):
     return x * (1-sigmoid(x))
--- functions/losses.py
+++ functions/losses.py
@@ -1,5 +1,7 @@
 import numpy as np
+from numba import njit
 
+@njit(cache=True, fastmath=True)
 def binary_crossentropy(y_real, y_pred):
     extra = 1e-10 #Against division by zero
     return -np.mean(y_real * np.log(y_pred + extra) + (1-y_real) * np.log(1-y_pred + extra))
