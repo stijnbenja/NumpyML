5b8cc10262757dfc255618ef24a21d243c3a7b20
Stijn <stijn@forcai.eu>
2021-10-03 11:49:44 +0200

    fixed annoying formatting

--- functions/activation.py
+++ functions/activation.py
@@ -6,5 +6,5 @@ def sigmoid(x):
     small_out = np.where(big_out < -100, -100, big_out)
     return 1 / (1 + np.exp(-small_out))
 
-
-def dSigmoid(x): return x * (1-sigmoid(x))
+def dSigmoid(x):
+    return x * (1-sigmoid(x))
--- functions/preprocess.py
+++ functions/preprocess.py
@@ -1,12 +1,6 @@
 import numpy as np
 
 # Normalisation & standardization functions
-
-
-def min_max(serie): return (serie - np.min(serie)) / \
-    (np.max(serie) - np.min(serie))
-def mean_normalization(serie): return (
-    serie - np.mean(serie)) / (np.max(serie) - np.min(serie))
-
-
+def min_max(serie): return (serie - np.min(serie)) / (np.max(serie) - np.min(serie))
+def mean_normalization(serie): return (serie - np.mean(serie)) / (np.max(serie) - np.min(serie))
 def standardization(serie): return (serie - np.mean(serie)) / np.std(serie)
