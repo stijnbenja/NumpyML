a9d1da9f1ef25451e68bdff200205768637b01de
Stijn <stijn@forcai.eu>
2021-10-03 17:12:10 +0200

    Added splitter

--- functions/preprocess.py
+++ functions/preprocess.py
@@ -9,3 +9,20 @@ def mean_normalization(serie):
 
 def standardization(serie): 
     return (serie - np.mean(serie)) / np.std(serie)
+
+def split(X, Y, train_size=0.8):
+    
+    cut_place = int(train_size * X.shape[1])
+    
+    x_train = X[ : , :cut_place ]
+    x_test  = X[ : , cut_place: ]
+    
+    try:
+        y_train = Y[ : , :cut_place ] #Generates error if it is an single array
+        y_test  = Y[ : , cut_place: ]
+    except:
+        y_train = Y[:cut_place]
+        y_test  = Y[cut_place:]
+   
+    return x_train, y_train, x_test, y_test
+    
