58d7ea7fed18d2b8acb241dbc8b32b9717b9b624
Stijn <stijn@forcai.eu>
2021-10-03 12:08:44 +0200

    added comment

--- functions/activations.py
+++ functions/activations.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def sigmoid(x):
-    ceiling = 100
+    ceiling = 100 #Against multiplication overflow
     big_out = np.where(x > ceiling, ceiling, x)
     small_out = np.where(big_out < -ceiling, -ceiling, big_out)
     return 1 / (1 + np.exp(-small_out))
