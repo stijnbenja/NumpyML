45271d9add051fc8de197f6ec1712bfcd41694da
Stijn <stijn@forcai.eu>
2021-10-04 14:16:08 +0200

    simple debug

--- functions/losses.py
+++ functions/losses.py
@@ -19,6 +19,9 @@ def binary_crossentropy(y_real, y_pred):
     extra = 1e-10 #Against division by zero
     return -np.mean(y_real * np.log(y_pred + extra) + (1-y_real) * np.log(1-y_pred + extra))
 
+
+
+
 def hinge(y_real, y_pred):
     pass
 
